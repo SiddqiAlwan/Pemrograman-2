d8a37a54551a4e1f1ceeb13a509e138d918e3301
SiddqiAlwan <154455128+SiddqiAlwan@users.noreply.github.com>
2024-03-09 10:56:49 +0700

    Add files via upload

---
+++ p1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title> Kesan Pertama Belajar PHP</title>
+</head>
+<body>
+<?php
+print "<font face='arial' size=6 color='black'> kesan petama saya ketika belajar bahasa program php ternyata kode yang di gunakan tidak terlalu rumit<br>";
+echo "bahasa yang digunakan tidak terlalu jauh berbeda dengan kode program html yang dapat mudah di mengerti </font>";
+?>
+<h3>TERIMAKASIH</h3>
+
+<?php
+print "<pre>";
+print "<center><b><font size = 20 color='red'>BIODATA</font></b></center>";
+echo "<hr size=4 width=74% color='blue'>";
+print "<b><font size = 14 color='blue'> Nama    : Muhammad Shiddqi Alwan </b></font><br>";
+print "<font size = 14 color='blue'> Email   : alwan.siddqi2508@gmail.com</font><br>";
+print "<font size = 14 color='blue'> Jurusan : Teknik Informatika </font><br>";
+print "<font size = 14 color='blue'> Hobby   : Bermain Gitar </font><br>
+</font><br></pre>";
+?>
+</body>
+</html>
